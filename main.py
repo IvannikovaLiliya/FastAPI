830058738febf570f319210058beab0bba93f86a
Lilya <ivannikova.liliya@yandex.ru>
2023-11-05 16:07:48 +0300

    Добавлен код для сервиса

---
+++ main.py
@@ -0,0 +1,95 @@
+from enum import Enum
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel, ValidationError
+
+app = FastAPI()
+
+
+class DogType(str, Enum):
+    terrier = "terrier"
+    bulldog = "bulldog"
+    dalmatian = "dalmatian"
+
+
+class Dog(BaseModel):
+    name: str
+    pk: int
+    kind: DogType
+
+
+class Timestamp(BaseModel):
+    id: int
+    timestamp: int
+
+
+dogs_db = {
+    0: Dog(name='Bob', pk=0, kind='terrier'),
+    1: Dog(name='Marli', pk=1, kind="bulldog"),
+    2: Dog(name='Snoopy', pk=2, kind='dalmatian'),
+    3: Dog(name='Rex', pk=3, kind='dalmatian'),
+    4: Dog(name='Pongo', pk=4, kind='dalmatian'),
+    5: Dog(name='Tillman', pk=5, kind='bulldog'),
+    6: Dog(name='Uga', pk=6, kind='bulldog')
+}
+
+post_db = [
+    Timestamp(id=0, timestamp=12),
+    Timestamp(id=1, timestamp=10)
+]
+
+
+@app.get('/')
+# возвращаем списком post_db для проверки работы метода /post
+def root():
+    return post_db
+
+
+@app.post('/post')
+# создаем новую запись в post_db и возвращаем её в случае успешного создания
+def get_post():
+    try:
+        last_timestamp = post_db[-1]
+        new_row = Timestamp(id=last_timestamp.id + 1, timestamp=last_timestamp.timestamp + 1)
+    except IndexError:
+        new_row = Timestamp(id=1, timestamp=1)
+    post_db.append(new_row)
+    return new_row
+
+
+@app.get('/dog')
+# возвращаем инофрмацию о всех собаках вводимой породы, иначе ошибку
+def get_dogs(kind: DogType):
+    dog_list = [dog for dog in dogs_db.values() if dog.kind == kind]
+    return dog_list
+
+
+@app.get('/dogs')
+# возвращаем инофрмацию о всех собаках в питомнике
+def get_dogs_all():
+    return dogs_db
+
+
+@app.post('/dog', response_model=Dog, summary='Create Dog')
+# создаем новую запись в dogs_db и возвращаем её в случае успешного создания
+def create_dog(dog: Dog):
+    if dog.pk in dogs_db:
+        raise HTTPException(status_code=422, detail="Данный PK занят!")
+    dogs_db[dog.pk] = dog
+    return dog
+
+
+@app.get("/dog/{pk}")
+def get_dog_by_pk(pk: int):
+    if pk not in dogs_db:
+        raise HTTPException(status_code=422, detail=f"Собака по данному PK ({pk}) не найдена!")
+    return dogs_db[pk]
+
+
+@app.patch("/dog/{pk}")
+def update_dog(pk: int, dog: Dog):
+    if pk not in dogs_db:
+        raise HTTPException(status_code=422, detail=f"Собака по данному PK ({pk}) не найдена!")
+    elif dog.pk != pk:
+        raise HTTPException(status_code=422, detail=f"Нельзя менять pk!")
+    dogs_db[pk] = dog
+    return dog
